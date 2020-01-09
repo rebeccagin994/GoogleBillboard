603b98f489186126951da0ea2eb8ea38591bb913
Rebecca Gin <ReGin@sfusd.edu>
2020-01-09 08:48:30 -0800

    Add first name to idex.html

--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class GoogleBillboard extends PApplet {
+
 
 public final static String e = "27182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435"; 
 
@@ -73,4 +89,13 @@ public boolean isPrime(double dNum){
    return true; 
 
 
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "GoogleBillboard" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
